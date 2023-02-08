75f8407a5c5ffc1fda9dc6a5652445172acdf8ab
neto112 <cristianorneto@gmail.com>
2023-02-08 20:47:36 -0300

    Component form development

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
   <div>
     <!-- INICIO NAVBAR -->
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <div class="container">
-        <a class="navbar-brand order-md-last" href="">
-          CRUD Vue.js + PostgreSQL + Node.js
-        </a>
-      </div>
+      <a class="navbar-brand order-md-last">CRUD Vue.js + PostgreSQL + Node.js</a>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Create New Employee</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/list-employees">List All</router-link>
+        </li>
+      </ul>
     </nav>
     <!-- FIM NAVBAR -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
   </div>
   <router-view/>
 </template>
--- src/components/create-employee/CreateEmployeeComponent.vue
+++ src/components/create-employee/CreateEmployeeComponent.vue
@@ -1,19 +1,95 @@
 <template>
-  <div class="row justify-content-center">
-    <h1>Create New Employee</h1>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h3>Add Employee</h3>
+      </div>
+      <div class="card-body">
+        <form v-on:submit.prevent="handleSubmitForm()">
+          <!-- Employee Name -->
+          <div class="form-group">
+            <label class="font-weight-bold">Employee Name</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Type Your Name"
+              v-model="employee.name"
+            />
+          </div>
+          <!-- Job Role -->
+          <div class="form-group">
+            <label class="font-weight-bold">Job Role</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Type Your Job Role"
+              v-model="employee.job_role"
+              required
+            />
+          </div>
+          <!-- Salary -->
+          <div class="form-group">
+            <label class="font-weight-bold">Salary</label>
+            <input
+              type="number"
+              class="form-control"
+              placeholder="Type Your Salary"
+              v-model="employee.salary"
+              required
+            />
+          </div>
+          <!-- Birth -->
+          <div class="form-group">
+            <label class="font-weight-bold">Birth</label>
+            <input
+              type="number"
+              class="form-control"
+              placeholder="YYYY/MM/DD"
+              v-model="employee.birth"
+              required
+            />
+          </div>
+          <!-- Employee Registration -->
+          <div class="form-group">
+            <label class="font-weight-bold">Employee Registration</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Employee Registration"
+              v-model="employee.employee_registration"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary" type="submit">+ Employee</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+    name: 'CreateEmployeeComponent',
+  },
   data() {
     return {
-
+      employee: {
+        name: '',
+        job_role: '',
+        salary: '',
+        birth: '',
+        employee_registration: '',
+      },
     };
+  },
+  methods: {
+    handleSubmitForm() {},
   },
 };
 </script>
 
 <style>
-
 </style>
